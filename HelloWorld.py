dd4fb72e7180fae03cfd8129c87a8ac28e254410
jfongyang <85820155+jfongyang@users.noreply.github.com>
2021-06-14 01:33:58 +0800

    del info*.html

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,19 +7,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/info.html')
-def info():
-    return 'info.html!'
-
-@app.route('/info2.html')
-def info2():
-    return 'Welcome to my world!'
-
-@app.route('/info3.html')
-def info3():
-    return 'this is info3.html'
-@app.route('/info4.html')
-def info4():
-    return 'this is info4.html'
 if __name__ == '__main__':
     app.run()
